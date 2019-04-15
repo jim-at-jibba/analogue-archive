99d2cf362a45f10587ee370ea8fbfed3039c9aa3
jim-at-gravitywell <james.best@gravitywell.co.uk>
2019-04-15 07:53:36 +0100

    Added react amplify package and auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Amplify from 'aws-amplify';
+import awsmobile from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
 
+Amplify.configure(awsmobile);
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>Hello</p>
       </div>
     );
   }
 }
 
-export default App;
+export default withAuthenticator(App);
